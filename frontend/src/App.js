a99441882ab4dceddad9bbfb2b5e6e8f25875a84
Aditya Gupta <adityagupta765469@gmail.com>
2022-07-12 13:43:19 +0530

    add rainbowkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,52 @@
-import './App.css';
+import "./App.css";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import Calendar from "./components/Calendar";
+import { useState } from "react";
+
+const { chains, provider } = configureChains(
+  [chain.rinkeby],
+  [
+    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 function App() {
-  const connect = () => {
-    alert('connect');
-  }
+  const [account, setAccount] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Calzy</h1>
-        <div id="slogan">Web3 Appointment Scheduling</div>
-        <button onClick={connect}>Connect Wallet</button>
-      </header>
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ConnectButton />
+      </RainbowKitProvider>
+    </WagmiConfig>
+   
   );
 }
 
