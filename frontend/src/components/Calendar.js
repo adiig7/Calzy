97690a2ce13053f2b46f970c2afd485e6be388db
Aditya Gupta <adityagupta765469@gmail.com>
2022-07-23 17:40:49 +0530

    new commit

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import abi from "../abis/Calzy.json";
+
 import {
   ViewState,
   EditingState,
@@ -11,11 +14,12 @@ import {
   Appointments,
   AppointmentForm,
 } from "@devexpress/dx-react-scheduler-material-ui";
+import { ethers } from 'hardhat';
 
 const schedulerData = [
   {
     startDate: '2022-07-18T09:45',
-    endDate: '2022-07-18T10:45', 
+    endDate: '2022-07-18T10:45',
     title: 'Meeting with Dhrumi'
   },
   {
@@ -23,13 +27,30 @@ const schedulerData = [
     endDate: '2022-07-19T10:45',
     title: 'Meeting with Dhrumi'
   }
-]
+];
 
+const saveAppointment = (data) => {
+  console.log("Appointment saved");
+  console.log(data);
+}
+
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractABI = abi.abi;
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const contract = new ethers.providers.Web3Provider(contractAddress, contractABI);
 const Calendar = () => {
-  const saveAppointment = (data) => {
-    console.log("Appointment saved");
-    console.log(data);
-  }
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [showAdmin, setShowAdmin] = useState(false);
+  const [rate, setRate] = useState();
+
+  // const getData =async () => {
+  //   const rate = await contract.getRate();
+  //   console.log(rate);
+  // }
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
   return (
     <div id="calendar">
       <Scheduler data={schedulerData}>
